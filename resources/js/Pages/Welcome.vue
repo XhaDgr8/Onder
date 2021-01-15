2d23aab50a972875770155068bd2aa10fdc7f125
XhaDgr8 <stfox003@gmail.com>
2021-01-15 15:50:50 +0500

    Laravel Vue Js with Tailwind and vue-tailwind components

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
+                Dashboard
+            </inertia-link>
+
+            <template v-else>
+                <inertia-link :href="route('login')" class="text-sm text-gray-700 underline">
+                    Login
+                </inertia-link>
+
+                <inertia-link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
+                    Register
+                </inertia-link>
+            </template>
+        </div>
+
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
+
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">{{testing}}</div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" class="underline">Forge</a>, <a href="https://vapor.laravel.com" class="underline">Vapor</a>, <a href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io" class="underline">Envoyer</a> help you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>, <a href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                <div class="text-center text-sm text-gray-500 sm:text-left">
+                    <div class="flex items-center">
+                        <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
+                            <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                        </svg>
+
+                        <a href="https://laravel.bigcartel.com" class="ml-1 underline">
+                            Shop
+                        </a>
+
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
+                            <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                        </svg>
+
+                        <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
+                            Sponsor
+                        </a>
+                    </div>
+                </div>
+
+                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .bg-gray-100 {
+        background-color: #f7fafc;
+        background-color: rgba(247, 250, 252, var(--tw-bg-opacity));
+    }
+
+    .border-gray-200 {
+        border-color: #edf2f7;
+        border-color: rgba(237, 242, 247, var(--tw-border-opacity));
+    }
+
+    .text-gray-400 {
+        color: #cbd5e0;
+        color: rgba(203, 213, 224, var(--tw-text-opacity));
+    }
+
+    .text-gray-500 {
+        color: #a0aec0;
+        color: rgba(160, 174, 192, var(--tw-text-opacity));
+    }
+
+    .text-gray-600 {
+        color: #718096;
+        color: rgba(113, 128, 150, var(--tw-text-opacity));
+    }
+
+    .text-gray-700 {
+        color: #4a5568;
+        color: rgba(74, 85, 104, var(--tw-text-opacity));
+    }
+
+    .text-gray-900 {
+        color: #1a202c;
+        color: rgba(26, 32, 44, var(--tw-text-opacity));
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .dark\:bg-gray-800 {
+            background-color: #2d3748;
+            background-color: rgba(45, 55, 72, var(--tw-bg-opacity));
+        }
+
+        .dark\:bg-gray-900 {
+            background-color: #1a202c;
+            background-color: rgba(26, 32, 44, var(--tw-bg-opacity));
+        }
+
+        .dark\:border-gray-700 {
+            border-color: #4a5568;
+            border-color: rgba(74, 85, 104, var(--tw-border-opacity));
+        }
+
+        .dark\:text-white {
+            color: #fff;
+            color: rgba(255, 255, 255, var(--tw-text-opacity));
+        }
+
+        .dark\:text-gray-400 {
+            color: #cbd5e0;
+            color: rgba(203, 213, 224, var(--tw-text-opacity));
+        }
+    }
+</style>
+
+<script>
+    export default {
+        props: {
+            canLogin: Boolean,
+            canRegister: Boolean,
+            laravelVersion: String,
+            phpVersion: String,
+            testing: String,
+        }
+    }
+</script>
